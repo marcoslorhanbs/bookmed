6c70f99ca3e30bbb54dd9cbaeae1cfb014f6d1ab
Marcos Lorhan <marcoslorhanbs@hotmail.com>
2025-11-29 08:21:39 -0300

    Allowed Hosts Config Direct

--- BookMed/settings.py
+++ BookMed/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = os.environ.get("SECRET_KEY", "chave-insegura-dev")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "")]
+ALLOWED_HOSTS = ["bookmed.onrender.com"]
 
 
 DATABASES = {
