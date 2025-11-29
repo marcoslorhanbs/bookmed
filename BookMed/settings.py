7e6b04f048a1fe2ae1e40cc65eace4cbbddfd724
Marcos Lorhan <marcoslorhanbs@hotmail.com>
2025-11-29 07:43:12 -0300

    major changes in settings for deploy

--- BookMed/settings.py
+++ BookMed/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = os.environ.get("SECRET_KEY", "chave-insegura-dev")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["seu-app.onrender.com"]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "")]
+
 
 DATABASES = {
     "default": dj_database_url.config(
