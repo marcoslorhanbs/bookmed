51e0f3c2caee81be870c7f3c308d58300e34eecf
Marcos Lorhan <marcoslorhanbs@hotmail.com>
2025-11-29 12:32:05 -0300

    Static files & Admin UI Fixes

--- BookMed/settings.py
+++ BookMed/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
